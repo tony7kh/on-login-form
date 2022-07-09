38b938c491063e492d31fd752789c35d768bf3db
tony7kh <antonkhizhuk1981@gmail.com>
2022-07-09 19:13:20 +0300

    on-login-form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
+const LoginForm = ({onLogin}) => {
+  const [login, setLogin] = useState("login");
+  const [password, setPassword] = useState("password");
+  return (
+    <>
+      <input
+        type="text"
+        placeholder={login}
+        onChange={(e) => setLogin(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <div>{password.length > 8 ? "Good password" : "Password is too short"}</div>
+      <button
+        onClick={() => onLogin({ login, password})}
+        disabled={
+          (login.length > 3 ? false : true) ||
+          (password.length > 8 ? false : true)
+        }
+      >
+        login
+      </button>
+    </>
+  );
+};
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <LoginForm 
+        onLogin={({login,password}) => alert(`Твой логин${login}. А еще я знаю твой пароль: ${password}. Ахаххахахха))))`)}
+      />
     </div>
   );
 }
-
 export default App;
